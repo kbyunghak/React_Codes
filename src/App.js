c16185131d40249501679173146ad1c39f7918b1
AndrewKim <andrew.kim@ballard.com>
2024-10-11 22:41:12 -0700

    Chapter #3 how to user component

--- src/App.js
+++ src/App.js
@@ -24,8 +24,10 @@ function App() {
   let posts = 'React Blog';
   let [Title, setTitle] = useState('React Lessons');  
   let [Ch1Title, setCh1Title] = useState(['Chapter #1', 'Check differences between state and variables']);  
+  let [Ch1Homework, setCh1Homework] = useState(['Chapter #1 - Homework', 'Add 2 items under ul tags']);  
   let [Ch2Title, setCh2Title] = useState(['Chapter #2', 'Eventlistener with thumbs up button']);  
-  let [Homework, setHomework] = useState(['Homework', 'Add a button for updating chapter2 title']);  
+  let [Ch2Homework, setCh2Homework] = useState(['Chapter #2 - Homework', 'Add a button for updating chapter2 title']);  
+  let [Ch3Title, setCh3Title] = useState(['Chapter #3', 'How to use Component']);    
   let [numOfClick, updateNOC] = useState(0);
   const [currentDate, setCurrentDate] = useState(getDate());
    
@@ -48,14 +50,21 @@ function App() {
         <ul>        
           <li> { Ch1Title[1] } </li>
         </ul> 
+        <li> { Ch1Homework[0] } </li>
+        <ul>
+          <li>
+            { Ch1Homework[1] }            
+          </li>
+        </ul>      
+
         <li> { Ch2Title[0] } </li>
         <ul>        
           <li> { Ch2Title[1] } </li>
-        </ul>    
-        <li> { Homework[0] } </li>
+        </ul>  
+        <li> { Ch2Homework[0] } </li>
         <ul>
           <li>
-            { Homework[1] }            
+            { Ch2Homework[1] }            
           </li>
           <li>
             <button onClick={ () => { setCh2Title(['Chapter3', 'Move on to the next Chapter #3'])}}> Homework - Rename Title(1)</button>
@@ -63,14 +72,41 @@ function App() {
             <button onClick={ renameTitle }> Homework - Rename Title(2)</button>
           </li>
           </ul>      
-        </ol>      
-        {/* <p>{ a }</p>
-        <p>{ b }</p> */}
+
+         <li> { Ch3Title[0] } </li>
+        <ul>        
+          <li> { Ch3Title[1] } </li>
+        </ul>  
+     
+
+        </ol>
         <p>Published on {currentDate}</p>
         <hr/>
+
+        <Modal/>
       </div>
     </div>
   );
+}
+
+//CREATE COMPONENTS
+//1. create a funtion 
+//2. function name should start with upper case 
+//3. <></> 
+//4. easily maintain
+//5. target? 
+//   repeatable contents, often update HTML UI, create other pages
+//6. disadvantage? difficult to use state
+function Modal() {
+  return (
+    <> 
+      <div className='modal'>
+        <h2>Title</h2>
+        <p>Date</p>
+        <p>Details</p>
+      </div>
+    </>
+  )
 }
 
 export default App;
