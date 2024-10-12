4419fcfe2fa587b44eebdd2607281acd8f980451
AndrewKim <andrew.kim@ballard.com>
2024-10-11 20:44:05 -0700

    commit React 2nd class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function getDate() {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  return `${month}/${date}/${year}`;
+}
+
 function App() {
+  var [a, b] = [10, 100]; // a= 10, b = 100
+  //Destructuring
+  let posts = 'React Blog';
+  let [Title, setTitle] = useState('React Lessons');  
+  let [Ch1Title, setCh1Title] = useState(['Chapter #1', 'Check differences between state and variables']);  
+  let [Ch2Title, setCh2Title] = useState(['Homework', 'Add 2 items under ul tags', 'Add 2 items under ul tags']);  
+  
+
+  const [currentDate, setCurrentDate] = useState(getDate());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ='App'>
+      <div className='black-nav'>
+        <div> { posts } </div>
+      </div>
+      <div className='list'>
+       <h3> { Title } </h3>    
+       <ol>
+        <li> { Ch1Title[0] } </li>
+        <ul>        
+          <li> { Ch1Title[1] } </li>
+        </ul> 
+        <li> { Ch2Title[0] } </li>
+        <ul>        
+          <li> { Ch2Title[1] } </li>
+          <li> { Ch2Title[2] } </li>
+        </ul>          
+        </ol>      
+       
+        <p>Published on {currentDate}</p>
+        <hr/>
+      </div>
     </div>
   );
 }
