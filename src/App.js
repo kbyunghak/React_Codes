e61f2a3f12fe6b6c9990780146a7e5759ec960e5
AndrewKim <andrew.kim@ballard.com>
2024-10-11 21:37:10 -0700

    Commit React event listener

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
+let numOfClick = 0;
 function getDate() {
   const today = new Date();
   const month = today.getMonth() + 1;
@@ -8,25 +9,32 @@ function getDate() {
   const date = today.getDate();
   return `${month}/${date}/${year}`;
 }
+function addNum() {
+  alert('click' + numOfClick); 
+  
+  numOfClick++;
+}
 
 function App() {
-  var [a, b] = [10, 100]; // a= 10, b = 100
+  
   //Destructuring
+  //var [a, b] = [10, 100]; // a= 10, b = 100
+ 
   let posts = 'React Blog';
   let [Title, setTitle] = useState('React Lessons');  
   let [Ch1Title, setCh1Title] = useState(['Chapter #1', 'Check differences between state and variables']);  
-  let [Ch2Title, setCh2Title] = useState(['Homework', 'Add 2 items under ul tags', 'Add 2 items under ul tags']);  
-  
-
+  let [Ch2Title, setCh2Title] = useState(['Chapter #2', 'Eventlistener with thumbs up button']);  
+  let [Homework, setHomework] = useState(['Homework', 'Add a button for updating chapter2 title']);  
+  let [numOfClick, updateNOC] = useState(0);
   const [currentDate, setCurrentDate] = useState(getDate());
-
+  
   return (
     <div className ='App'>
       <div className='black-nav'>
         <div> { posts } </div>
       </div>
       <div className='list'>
-       <h3> { Title } </h3>    
+       <h3> { Title } <span onClick={ () => { updateNOC(numOfClick++);}}>👍</span> { numOfClick } </h3>    
        <ol>
         <li> { Ch1Title[0] } </li>
         <ul>        
@@ -35,10 +43,19 @@ function App() {
         <li> { Ch2Title[0] } </li>
         <ul>        
           <li> { Ch2Title[1] } </li>
-          <li> { Ch2Title[2] } </li>
-        </ul>          
+        </ul>    
+        <li> { Homework[0] } </li>
+        <ul>
+          <li>
+            { Homework[1] }            
+          </li>
+          <li>
+            <button onClick={ () => { setCh2Title(['Chapter3', 'Move on to the next Chapter #3'])}}>Homework Button</button>
+          </li>
+          </ul>      
         </ol>      
-       
+        {/* <p>{ a }</p>
+        <p>{ b }</p> */}
         <p>Published on {currentDate}</p>
         <hr/>
       </div>
